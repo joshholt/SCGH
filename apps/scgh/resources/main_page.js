c2d39ebab7000b8313d137101509074f2d7b42d0
Josh Holt <holt.josh@gmail.com>
2011-02-15 23:11:16 -0500

    Toolbars, Responders, and Style

--- apps/scgh/resources/main_page.js
+++ apps/scgh/resources/main_page.js
@@ -11,11 +11,41 @@ Scgh.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'repoSection issuesSection'.w(),
-    
+    childViews: 'topBar repoSection issuesSection'.w(),
+
+    topBar: SC.ToolbarView.extend({
+      childViews: 'left center right'.w(),
+
+      left: SC.View.extend({
+        layout: {top: 0, bottom: 0, left: 0, width: 225},
+        childViews: 'appTitle'.w(),
+
+        appTitle: SC.LabelView.extend({
+          layout: {left: 10, centerY: 0, height: 18, width: 100},
+          value: "Tasks (for GitHub)"
+        })
+      }),
+
+      center: SC.View.extend({
+        layout: {top: 0, bottom: 0, left: 226, right: 226}
+      }),
+
+      right: SC.View.extend({
+        layout: {top: 0, bottom: 0, right: 0, width: 225},
+        childViews: 'actionMenu'.w(),
+
+        actionMenu: SC.ButtonView.extend({
+          layout: {right: 10, centerY: 0, height: 24, width: 75},
+          titleMinWidth: 0,
+          title: "Account"
+        })
+      })
+    }),
+
     repoSection: SC.View.extend({
-      layout: { top: 0, left: 0, width: 225, bottom: 0 },
+      layout: { top: 33, left: 0, width: 225, bottom: 0 },
       childViews: 'repoList'.w(),
+      classNames: 'source-list'.w(),
 
       repoList: SC.ScrollView.extend({
         contentView: SC.ListView.extend({
@@ -23,17 +53,33 @@ Scgh.mainPage = SC.Page.design({
           selectionBinding: 'Scgh.projectsController.selection',
           contentValueKey: 'name',
           contentUnreadCountKey: 'open_issues',
+          exampleView: SC.ListItemView.extend({ classNames: 'scgh-list-item'.w() }),
+
+          willLoseFirstResponder: function(r) {
+            var sel = this.getPath('selection.firstObject');
+            var ret = sc_super();
+
+            if(sel) {
+              this.itemViewForContentObject(sel).$().addClass('inactive');
+            }
+            return ret;
+          },
+
+          didBecomeFirstResponder: function() {
+            var sel = this.getPath('selection.firstObject');
+            var ret = sc_super();
+
+            if(sel) {
+              this.itemViewForContentObject(sel).$().removeClass('inactive');
+            }
+            return ret;
+          },
 
           keyDown: function(evt) {
             if(evt.keyCode !== 39) {
               sc_super();
-            }
-            else {
-              SC.RunLoop.begin();
-              Scgh.issuesController.selectObject(Scgh.issuesController.get('firstObject'));
-              var nr =  Scgh.getPath('mainPage.issuesList');
-              if (nr) nr.becomeFirstResponder();
-              SC.RunLoop.end();
+            } else {
+              Scgh.sendEvent('changeFocus');
               return YES;
             }
             return NO;
@@ -44,7 +90,7 @@ Scgh.mainPage = SC.Page.design({
     }),
 
     issuesSection: SC.View.extend({
-      layout: { top: 0, left: 225, right: 0, bottom: 0 },
+      layout: { top: 33, left: 226, right: 0, bottom: 0 },
       childViews: 'issuesList issueDetails'.w(),
 
       issuesList: SC.ScrollView.extend({
@@ -53,17 +99,34 @@ Scgh.mainPage = SC.Page.design({
           contentBinding: 'Scgh.issuesController.arrangedObjects',
           selectionBinding: 'Scgh.issuesController.selection',
           contentValueKey: 'title',
+          exampleView: SC.ListItemView.extend({ classNames: 'scgh-list-item'.w(), showAlternatingRows: true }),
+
+          willLoseFirstResponder: function(r) {
+            var sel = this.getPath('selection.firstObject');
+            var ret = sc_super();
+
+            if(sel) {
+              this.itemViewForContentObject(sel).$().addClass('inactive');
+            }
+            return ret;
+          },
+
+          didBecomeFirstResponder: function() {
+            var sel = this.getPath('selection.firstObject');
+            var ret = sc_super();
+
+            if(sel) {
+              this.itemViewForContentObject(sel).$().removeClass('inactive');
+            }
+            return ret;
+          },
+
 
           keyDown: function(evt) {
-            console.log(evt.keyCode);
             if(evt.keyCode !== 37) {
               sc_super();
-            }
-            else {
-              SC.RunLoop.begin();
-              var nr =  Scgh.getPath('mainPage.repoList');
-              if (nr) nr.becomeFirstResponder();
-              SC.RunLoop.end();
+            } else {
+              Scgh.sendEvent('changeFocus');
               return YES;
             }
             return NO;
